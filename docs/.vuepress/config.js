d18034d8c5c6a92fc10cce38825c4b25f9af741c
Justin Southern Isles <2410377391@qq.com>
2022-08-19 17:13:34 +0800

    Fix config.js base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,7 @@ module.exports = {
 
   title: "SVFI Doc",
   description: 'SVFI用户使用文档',
-  base: '/SVFI-DOC/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+  base: '', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: '/img/svfi.ico' }], //favicons，资源放在public文件夹
     ['meta', { name: 'keywords', content: 'vuepress,theme,blog,vdoing' }],
